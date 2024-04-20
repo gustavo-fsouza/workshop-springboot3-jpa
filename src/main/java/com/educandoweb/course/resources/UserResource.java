362318a857c92b045c7a822009a4e14bdf0756ca
Gustavo de Souza <gustavo.fsouza@hotmail.com>
2024-04-20 10:58:02 -0300

    User update

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -49,5 +50,11 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PatchMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user){
+		user = service.update(id, user);
+		return ResponseEntity.ok().body(user);
 	}
 }

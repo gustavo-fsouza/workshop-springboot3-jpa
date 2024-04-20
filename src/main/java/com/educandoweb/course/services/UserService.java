d2fd275d3bf2a51f6b00581c1be83e466c28ddef
Gustavo de Souza <gustavo.fsouza@hotmail.com>
2024-04-20 11:51:37 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -36,24 +38,36 @@ public class UserService {
 		if(repository.existsById(id) == false){ 
 			throw new ResourceNotFoundException(id);
 		}
+		
 		try {
 			repository.deleteById(id);
 		}
 		catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		catch (EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
-		entity.setName(obj.getName());
-		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());
+		if(obj.getName() != null) {
+			entity.setName(obj.getName());
+		}
+		if(obj.getEmail() != null) {
+			entity.setEmail(obj.getEmail());
+		}
+		if(obj.getPhone() != null) {
+			entity.setPhone(obj.getPhone());
+		}
 	}
 }
